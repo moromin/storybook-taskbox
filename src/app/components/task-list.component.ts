e124eedfa05a9b9cd5b8e47150d64f8cd9d7f598
moromin <moromin933@gmai.com>
2022-08-23 21:25:12 +0900

    feat: Data

--- src/app/components/task-list.component.ts
+++ src/app/components/task-list.component.ts
@@ -1,42 +1,30 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-
-import { Task } from '../models/task.model';
+import { Component } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { ArchiveTask, PinTask } from '../state/task.state';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
 })
 export class TaskListComponent {
+  tasks$?: Observable<any>;
+
+  constructor(private store: Store) {
+    this.tasks$ = store.select((state) => state.taskbox.tasks);
+  }
+
   /**
-   * @ignore
-   * Component property to define ordering of tasks
+   * Component method to trigger the archiveTask event
    */
-  tasksInOrder: Task[] = [];
+  archiveTask(id: string) {
+    this.store.dispatch(new ArchiveTask(id));
+  }
 
-  /** Checks if it's in loading state */
-  @Input() loading = false;
-
-  /** Event to change the task to pinned */
-  // tslint:disable-next-line: no-output-on-prefix
-  @Output()
-  onPinTask = new EventEmitter<Event>();
-
-  /** Event to change the task to archived */
-  // tslint:disable-next-line: no-output-on-prefix
-  @Output()
-  onArchiveTask = new EventEmitter<Event>();
-
-  @Input()
-  set tasks(arr: Task[]) {
-    const initialTasks = [
-      ...arr.filter((t) => t.state === 'TASK_PINNED'),
-      ...arr.filter((t) => t.state !== 'TASK_PINNED'),
-    ];
-    const filteredTasks = initialTasks.filter(
-      (t) => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED'
-    );
-    this.tasksInOrder = filteredTasks.filter(
-      (t) => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED'
-    );
+  /**
+   * Component method to trigger the pinTask event
+   */
+  pinTask(id: string) {
+    this.store.dispatch(new PinTask(id));
   }
 }
